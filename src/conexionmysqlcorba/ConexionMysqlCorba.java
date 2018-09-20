27fe4c4ff93f9640959c63630c7dd1600fcc159e
Usuario <Usuario@Lenovo-PC>
2018-09-19 21:44:25 -0500

    Se crea clase Conexion

--- src/conexionmysqlcorba/ConexionMysqlCorba.java
+++ src/conexionmysqlcorba/ConexionMysqlCorba.java
@@ -2,6 +2,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+MYADMIN Crear tabla, en paquete agregar libreria MYSQL
+Crear clase Conexion.java
  */
 package conexionmysqlcorba;
 
@@ -15,7 +17,9 @@ public class ConexionMysqlCorba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // 
+        Conexion objConex=new Conexion();
+        objConex.conectar();
     }
     
 }
